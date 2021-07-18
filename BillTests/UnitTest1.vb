2210f567c62dcbb447a331c9ecc9755780a9e69b
lewandowskijakub <lewyjb@gmail.com>
2021-07-18 14:23:24 +0200

    Initial commit

---
+++ BillTests/UnitTest1.vb
@@ -0,0 +1,14 @@
+Imports NUnit.Framework
+
+Namespace BillTests
+    Public Class Tests
+        <SetUp>
+        Public Sub Setup()
+        End Sub
+
+        <Test>
+        Public Sub Test1()
+            Assert.Pass()
+        End Sub
+    End Class
+End Namespace
